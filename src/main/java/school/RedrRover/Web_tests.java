55fefc9846d4c5f9c4688302c87aecf87c5fcc3a
Eduard-QA <eduard.mickel@gmail.com>
2024-10-12 11:40:57 +0300

    uniTest

---
+++ src/main/java/school/RedrRover/Web_tests.java
@@ -0,0 +1,10 @@
+package school.RedrRover;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Web_tests {
+    public void testDriver(){
+        WebDriver driver = new ChromeDriver();
+    }
+}
