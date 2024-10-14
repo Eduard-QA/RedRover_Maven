bb3e89279ca2a81c5380e39d610bff717fa6c907
Eduard-QA <eduard.mickel@gmail.com>
2024-10-14 22:14:05 +0300

    added testGoogle

--- src/test/java/Lesson15.java
+++ src/test/java/Lesson15.java
@@ -8,9 +8,9 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class Lesson_15 {
+public class Lesson15 {
     @Test
-    public void test() {
+    public void testSelenium() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
 
@@ -31,6 +31,31 @@ public class Lesson_15 {
         WebElement message = driver.findElement(By.id("message"));
         Assert.assertEquals(message.getText(), "Received!");
 
+        driver.quit();
+    }
+
+    @Test
+    public void testGoogle() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.google.co.uk/");
+
+        System.out.println(driver.getTitle());
+
+        WebElement textBox = driver.findElement(By.className("gLFyf"));
+        textBox.sendKeys("Selenium");
+
+        Thread.sleep(1000);
+        WebElement submitButton = driver.findElement(By.className("gNO89b"));
+        submitButton.click();
+
+        WebElement downloadsLink = driver.findElement(By.linkText("Downloads"));
+        downloadsLink.click();
+
+        Assert.assertEquals(driver.getTitle(), "Downloads | Selenium");
         driver.quit();
     }
 }
